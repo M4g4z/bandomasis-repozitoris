57155b97eb7a07a1aa69fa3974882024ec76b7f1
M4g4z <74550912+M4g4z@users.noreply.github.com>
2020-11-16 17:45:10 +0200

    Create carK.java

---
+++ carK.java
@@ -0,0 +1,14 @@
+public class carK{
+
+     public static void main(String []args){
+        int x = 1;
+        while(x < 4) {
+        System.out.println("C 4 R K");
+        System.out.println("4 4");
+        System.out.println("R   R");
+        System.out.print("K     K");
+        System.out.println(" ");
+        x = x + 1;
+        }
+     }
+}
