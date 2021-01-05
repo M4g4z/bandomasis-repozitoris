9ca53ca470130fadb840659558ca881175e1e6a2
M4g4z <74550912+M4g4z@users.noreply.github.com>
2021-01-05 09:04:32 +0200

    Add files via upload

--- Uzduotis.java
+++ Uzduotis.java
@@ -1,14 +1,19 @@
+import java.util.Arrays;
+
 public class Uzduotis {
     public static void main(String[] args) {
     
         System.out.println(plotoSkaiciavimas(15, 18));
         System.out.println("^Plotas, mylios v");
         System.out.println(kmIMylias(8.05));
-
+        System.out.println();
         sukeisti(4, 7);
-
+        System.out.print("Sukeitimas per masyvus: ");
+        sukeitimas(4, 7);
+        System.out.println();
         int[] skMas = {-1337, 0, 1337, 2674, 4011};
-        System.out.println("vidurkis: " + vidurkis(skMas));
+        System.out.println();
+        System.out.println("Vidurkis: " + vidurkis(skMas));
     }
 
     public static double plotoSkaiciavimas(double ilgis, double plotis) {
@@ -33,17 +38,23 @@ public class Uzduotis {
         System.out.println("x = " + x + "  y = " + y);
     }
 
-    public static double vidurkis(int[] skMas) {
+    public static int[] sukeitimas(int sk1, int sk2) {
+        int[] sukeistieji = new int[2];
+        sukeistieji[0] = sk2;
+        sukeistieji[1] = sk1;
+        System.out.println(Arrays.toString(sukeistieji));
+        return sukeistieji;
+    }
 
+    public static double vidurkis(int[] skMas) {
         double avg = 0;
         int suma = 0;
 
         for (int i = 0; i < skMas.length; i++) {
-            suma = suma + skMas[i];
+            suma += skMas[i];
             avg = suma / skMas.length;
         }
         
         return avg;
     }
 }
-
